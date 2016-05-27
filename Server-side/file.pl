1ecb61e65a3bfb86b55d7e87adc8e10f491492f1
Jared Brees <j8t8b@yahoo.com>
2016-05-27 12:12:11 -0500

    lots of changes
    includes (non-exhaustive list):
    - spaces to tabs
    - naming changes
    - added RR1 status/vars

--- Server-side/file.pl
+++ Server-side/file.pl
@@ -10,12 +10,22 @@ use Switch;
 #&getArraysReady;
 &doFileRead;
 #&printFileStuff;
-&testCaseSwitch;
+&lineByLine;
 
-our @status;
-sub getArraysReady() {
-        our @status;
-}
+# 0. have "getter" get stuff, before this script runs
+# 1. Get variables ready
+# 2. Read everything in
+# 3. Make sure there's no %02 or %03
+# 4. Put it all in array @status
+# 5. Go line-by-line and grab statuses
+# 6. As going line by line and grabbing, store in hash
+# 7. Print stuff (parsed!) back out as necessary.
+# 8. Web pages will read written-out stuff as JavaScript
+
+
+#sub getArraysReady() {
+		our @status;
+#}
 
 #sub getHashesReady () {
         our %bridge_mainLeft;
@@ -41,54 +51,67 @@ sub getArraysReady() {
 
 
 sub doFileRead() {
-        open (my $fh, "<", "/opt/data/projstatus/dumpstatus.txt")
-                or die "Failed to open file $!\n";
+		open (my $fh, "<", "/opt/data/projstatus/dumpstatus.txt")
+				or die "Failed to open file $!\n";
 
-        while (my $x = <$fh>) {
-                chomp $x;
-                # the following two lines are redundant,
-                # since they should be caught by the "getter"
-                # script and Extron box, respectively.
-                $x =~ s/%02//g;
-                $x =~ s/%03//g;
-                push @status, $x;
-        }
+		while (my $x = <$fh>) {
+				chomp $x;
+				# the following two lines are redundant,
+				# since they should be caught by the "getter"
+				# script and Extron box, respectively.
+				$x =~ s/%02//g;
+				$x =~ s/%03//g;
+				push @status, $x;
+		}
 }
 
-sub printFileStuff() {
-        foreach my $x (@status) {
-        #       print $x;
-        #       print "\n"
-                if ($x =~ 'panasonicPJLink') {
-                print $x."\n";
-                } else {
-                print "not panasonic PJ Link \n";
-                } 
-        }
-}
+#sub printFileStuff() {
+#		foreach my $x (@status) {
+#				if ($x =~ 'panasonicPJLink') {
+#				print $x."\n";
+#				} else {
+#				print "not panasonic PJ Link \n";
+#				} 
+#		}
+#}
 
-sub testCaseSwitch() {
-        foreach my $x (@status) {
-                switch ($x) {
-                        #case m/bridge_mainLeft/ { print $x; }
-                        case m/bridge_mainLeft/ { %bridge_mainLeft = &parseLine($x); }
-                        #case m/bridge_mainLeft/ { &parseLine($x); }
-                }
-        }
+sub lineByLine() {
+		foreach my $x (@status) {
+				switch ($x) {
+						case m/bridge_mainLeft/ { %bridge_mainLeft = &parseLine($x); }
+						case m/bridge_mainRight/ { %bridge_mainRight = &parseLine($x); }
+						case m/bridge_mainCenter/ { %bridge_mainCenter = &parseLine($x); }
+						case m/bridge_foldbackCenter/ { %bridge_foldbackCenter = &parseLine($x); }
+						case m/chapel_mainSide/ { %chapel_mainSide = &parseLine($x); }
+						case m/chapel_mainCenter/ { %chapel_mainCenter = &parseLine($x); }
+						case m/chapel_foldbackCenter/ { %chapel_foldbackCenter = &parseLine($x); }
+						case m/gym_mainSide/ { %gym_mainSide = &parseLine($x); }
+						case m/well_mainCenter/ { %well_mainCenter = &parseLine($x); }
+						case m/rm101A_mainCenter/ { %rm101A_mainCenter = &parseLine($x); }
+						case m/rm101C_mainCenter/ { %rm101C_mainCenter = &parseLine($x); }
+						case m/rm102_mainCenter/ { %rm102_mainCenter = &parseLine($x); }
+						case m/rm104_mainCenter/ { %rm104_mainCenter = &parseLine($x); }
+						case m/rm128_mainCenter/ { %rm128_mainCenter = &parseLine($x); }
+						case m/rm212_mainCenter/ { %rm212_mainCenter = &parseLine($x); }
+						case m/rm214_mainCenter/ { %rm214_mainCenter = &parseLine($x); }
+						case m/rm216_mainCenter/ { %rm216_mainCenter = &parseLine($x); }
+						case m/rmRR1_mainCenter/ { %rmRR1_mainCenter = &parseLine($x); }
+				}
+		}
 }
 
 sub parseLine($) {
-        my $data = shift;
-        my($proj,$item,$type,$status) = split /,/, $data;
-        #print "projector: $proj\n";
-        my %hashName = (
-                proj => $proj,
-                item => $item,
-                type => $type,
-                status => $status,
-        );
-        return %hashName;
-        #print %hashName;
+		my $data = shift;
+		my($proj,$item,$type,$status) = split /,/, $data;
+		#print "projector: $proj\n";
+		my %hashName = (
+				proj => $proj,
+				item => $item,
+				type => $type,
+				status => $status,
+		);
+		return %hashName;
+		#print %hashName;
 }
 
 print $bridge_mainLeft{"status"}."\n";
