3320514c03a2129f842187556321333688c759d5
Jared Brees <j8t8b@yahoo.com>
2016-06-11 10:59:29 -0500

    updates
    updating stuff

--- Server-side/file.pl
+++ Server-side/file.pl
@@ -82,257 +82,125 @@ sub doFileRead() {
 #}
 
 sub lineByLine() {
-		foreach my $x (@data) {
-			&parseLine($x);
+	my $x;
+	foreach $x (@data) {
+		switch ($x) {
+			case m/bridge_mainLeft/ { %bridge_mainLeft = parseLine($x); }
+			case m/bridge_mainRight/ { %bridge_mainRight = parseLine($x); }
+			case m/bridge_mainCenter/ { %bridge_mainCenter = parseLine($x); }
+			case m/bridge_foldbackCenter/ { %bridge_foldbackCenter = parseLine($x); }
+			case m/chapel_mainSide/ { %chapel_mainSide = parseLine($x); }
+			case m/chapel_mainCenter/ { %chapel_mainCenter = parseLine($x); }
+			case m/chapel_foldbackSide/ { %chapel_foldbackSide = parseLine($x); }
+			case m/gym_mainSide/ { %gym_mainSide = parseLine($x); }
+			case m/well_mainCenter/ { %well_mainCenter = parseLine($x); }
+			case m/rm101A_mainCenter/ { %rm101A_mainCenter = parseLine($x); }
+			case m/rm101C_mainCenter/ { %rm101C_mainCenter = parseLine($x); }
+			case m/rm102_mainCenter/ { %rm102_mainCenter = parseLine($x); }
+			case m/rm104_mainCenter/ { %rm104_mainCenter = parseLine($x); }
+			case m/rm128_mainCenter/ { %rm128_mainCenter = parseLine($x); }
+			case m/rm212_mainCenter/ { %rm212_mainCenter = parseLine($x); }
+			case m/rm214_mainCenter/ { %rm214_mainCenter = parseLine($x); }
+			case m/rm216_mainCenter/ { %rm216_mainCenter = parseLine($x); }
+			case m/rmRR1_mainCenter/ { %rmRR1_mainCenter = parseLine($x); }
 		}
-#				switch ($x) {
-#						case m/bridge_mainLeft/ { %bridge_mainLeft = parseLine($x); }
-#						case m/bridge_mainRight/ { %bridge_mainRight = parseLine($x); }
-#						case m/bridge_mainCenter/ { %bridge_mainCenter = parseLine($x); }
-#						case m/bridge_foldbackCenter/ { %bridge_foldbackCenter = parseLine($x); }
-#						case m/chapel_mainSide/ { %chapel_mainSide = parseLine($x); }
-#						case m/chapel_mainCenter/ { %chapel_mainCenter = parseLine($x); }
-#						case m/chapel_foldbackCenter/ { %chapel_foldbackCenter = parseLine($x); }
-#						case m/gym_mainSide/ { %gym_mainSide = parseLine($x); }
-#						case m/well_mainCenter/ { %well_mainCenter = parseLine($x); }
-#						case m/rm101A_mainCenter/ { %rm101A_mainCenter = parseLine($x); }
-#						case m/rm101C_mainCenter/ { %rm101C_mainCenter = parseLine($x); }
-#						case m/rm102_mainCenter/ { %rm102_mainCenter = parseLine($x); }
-#						case m/rm104_mainCenter/ { %rm104_mainCenter = parseLine($x); }
-#						case m/rm128_mainCenter/ { %rm128_mainCenter = parseLine($x); }
-#						case m/rm212_mainCenter/ { %rm212_mainCenter = parseLine($x); }
-#						case m/rm214_mainCenter/ { %rm214_mainCenter = parseLine($x); }
-#						case m/rm216_mainCenter/ { %rm216_mainCenter = parseLine($x); }
-#						case m/rmRR1_mainCenter/ { %rmRR1_mainCenter = parseLine($x); }
-#				}
-#		}
+	}
 }
 #TODO: try something like
 #while ($item =~ m/(00|80|40|20|10|28|02|24|04|21|81|88)
 #$status=$statusHashname{$1}
 sub parseLine {
-		# declare some vars
-		our %hashName;
-		our ($status,$hoursAll,$hours1,$hours2,$hours3,$hours4);
-		# $data is whatever was passed to us
-		my $data = shift;
-		# split out the line from Extron box
-		#DEBUG
-		print $data;
-		print "\n";
-		
-		our ($proj,$item,$type,$value) = split /,/, $data;
-		#DEBUG
-		print 'proj '."$proj"."\n";
-		print 'item '."$item"."\n";
-		print 'type '."$type"."\n";
-		print 'value '."$value"."\n";
-		
-		# determine what item (power, lamp hours, etc) we're
-		# looking at, and act upon it.
-		switch ($item) {
-			case m/power/ {
-				switch ($type) {
-					case m/panasonicPJLink/ {
-						switch ($value) {
-							case m/000/ { $status = 'off'; }
-							case m/001/ { $status = 'on'; }
-						}
+	# declare some vars
+	our %hashName;
+	our ($power,$hoursAll,$hours1,$hours2,$hours3,$hours4);
+	# $data is whatever was passed to us
+	my $data = shift;
+	# split out the line from Extron box
+	our ($proj,$item,$type,$value) = split /,/, $data;
+	
+	# determine what item (power, lamp hours, etc) we're
+	# looking at, and act upon it.
+	switch ($item) {
+		case m/power/ {
+			switch ($type) {
+				case m/panasonicPJLink/ {
+					switch ($value) {
+						case m/000/ { $power = 'off'; }
+						case m/001/ { $power = 'on'; }
 					}
-					case m/sanyoPLC/ {
-						switch ($value) {
-							case m/00/ { $status = 'on'; }
-							case m/80/ { $status = 'off'; }
-							case m/40/ { $status = 'Countdown'; }
-							case m/20/ { $status = 'Cooling Down'; }
-							case m/10/ { $status = 'Power Malfunction'; }
-							case m/28/ { $status = 'Cooling down at the temperature anomaly'; }
-							case m/02/ { $status = 'Unable to receive the RC232C command.'; }
-							case m/24/ { $status = 'Cooling down at Power Management mode'; }
-							case m/04/ { $status = 'Power Management mode after Cooling down'; }
-							case m/21/ { $status = 'Cooling down after the projector is turned off when the lamps are out.'; }
-							case m/81/ { $status = 'Stand-by mode after Cooling down when the lamps are out.'; }
-							case m/88/ { $status = 'Stand-by mode after Cooling down at the temperature anomaly.'; }
-						}
+				}
+				case m/sanyoPLC/ {
+					switch ($value) {
+						case m/00/ { $power = 'on'; }
+						case m/80/ { $power = 'off'; }
+						case m/40/ { $power = 'Countdown'; }
+						case m/20/ { $power = 'Cooling Down'; }
+						case m/10/ { $power = 'Power Malfunction'; }
+						case m/28/ { $power = 'Cooling down at the temperature anomaly'; }
+						case m/02/ { $power = 'Unable to receive the RC232C command.'; }
+						case m/24/ { $power = 'Cooling down at Power Management mode'; }
+						case m/04/ { $power = 'Power Management mode after Cooling down'; }
+						case m/21/ { $power = 'Cooling down after the projector is turned off when the lamps are out.'; }
+						case m/81/ { $power = 'Stand-by mode after Cooling down when the lamps are out.'; }
+						case m/88/ { $power = 'Stand-by mode after Cooling down at the temperature anomaly.'; }
 					}
 				}
 			}
-			case m/lampHoursAll/ {
-				($hours1,$hours2,$hours3,$hours4) = split / /, $value;
-			}
-			case m/lampHours1/ {
-				$hours1 = $value;
-			}
-			case m/lampHours2/ {
-				$hours2 = $value;
-			}
-			case m/lampHours3/ {
-				$hours3 = $value;
-			}
-			case m/lampHours4/ {
-				$hours4 = $value;
-			}
 		}
-		#DEBUG
-		print $status;
-		print "\n";
-
+		case m/lampHoursAll/ {
+			($hours1,$hours2,$hours3,$hours4) = split / /, $value;
+		}
+		case m/lampHours1/ {
+			$hours1 = $value;
+		}
+		case m/lampHours2/ {
+			$hours2 = $value;
+		}
+		case m/lampHours3/ {
+			$hours3 = $value;
+		}
+		case m/lampHours4/ {
+			$hours4 = $value;
+		}
+	}
+		# initilize the hash we're about to use below..
+		# ..with some "dummy" data:
+		%hashName = (
+			proj => 'NO_DATA',
+			type => 'NO_DATA',
+			power => 'NO_DATA',
+		#	hoursAll => 'NO_DATA',
+			hours1 => 'NO_DATA',
+			hours2 => 'NO_DATA',
+			hours3 => 'NO_DATA',
+			hours4 => 'NO_DATA',
+		);
+		
 		# put data we have into a temporary
 		# hash, just for the fun of it:
+#		if (defined $proj) { $hashName{'proj'} = $proj; }
+#		if (defined $type) { $hashName{'type'} = $type; }
+#		if (defined $power) { $hashName{'power'} = $power; }
+#		if (defined $hoursAll) { $hashName{'hoursAll'} = $hoursAll; }
+#		if (defined $hours1) { $hashName{'hours1'} = $hours1; }
+#		if (defined $hours2) { $hashName{'hours2'} = $hours2; }
+#		if (defined $hours3) { $hashName{'hours3'} = $hours3; }
+#		if (defined $hours4) { $hashName{'hours4'} = $hours4; }
 		%hashName = (
 			proj => $proj,
 			type => $type,
-			status => $status,
+			power => $power,
 		#	hoursAll => $hoursAll,
 			hours1 => $hours1,
 			hours2 => $hours2,
 			hours3 => $hours3,
 			hours4 => $hours4,
 		);
+#		print %hashName;
 
-		# figure out what projector, and what data,
-		# needs to be put into the appropriate hash
-		switch ($proj) {
-			case m/bridge_mainLeft/ {
-				print "\n\n".'found bridge main left'."\n\n";
-				if (defined $status) { $bridge_mainLeft{'power'} = $status; }
-				if (defined $hours1) { $bridge_mainLeft{'hours1'} = $hours1; }
-				if (defined $hours2) { $bridge_mainLeft{'hours2'} = $hours2; }
-				if (defined $hours3) { $bridge_mainLeft{'hours3'} = $hours3; }
-				if (defined $hours4) { $bridge_mainLeft{'hours4'} = $hours4; }
-			}
-			case m/bridge_mainRight/ {
-				if (defined $status) { $bridge_mainRight{'power'} = $status; }
-				if (defined $hours1) { $bridge_mainRight{'hours1'} = $hours1; }
-				if (defined $hours2) { $bridge_mainRight{'hours2'} = $hours2; }
-				if (defined $hours3) { $bridge_mainRight{'hours3'} = $hours3; }
-				if (defined $hours4) { $bridge_mainRight{'hours4'} = $hours4; }
-			}
-			case m/bridge_mainCenter/ {
-				if (defined $status) { $bridge_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $bridge_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $bridge_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $bridge_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $bridge_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/bridge_foldbackCenter/ {
-				if (defined $status) { $bridge_foldbackCenter{'power'} = $status; }
-				if (defined $hours1) { $bridge_foldbackCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $bridge_foldbackCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $bridge_foldbackCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $bridge_foldbackCenter{'hours4'} = $hours4; }
-			}
-			case m/chapel_mainSide/ {
-				if (defined $status) { $chapel_mainSide{'power'} = $status; }
-				if (defined $hours1) { $chapel_mainSide{'hours1'} = $hours1; }
-				if (defined $hours2) { $chapel_mainSide{'hours2'} = $hours2; }
-				if (defined $hours3) { $chapel_mainSide{'hours3'} = $hours3; }
-				if (defined $hours4) { $chapel_mainSide{'hours4'} = $hours4; }
-			}
-			case m/chapel_mainCenter/ {
-				if (defined $status) { $chapel_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $chapel_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $chapel_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $chapel_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $chapel_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/chapel_foldbackSide/ {
-				if (defined $status) { $chapel_foldbackSide{'power'} = $status; }
-				if (defined $hours1) { $chapel_foldbackSide{'hours1'} = $hours1; }
-				if (defined $hours2) { $chapel_foldbackSide{'hours2'} = $hours2; }
-				if (defined $hours3) { $chapel_foldbackSide{'hours3'} = $hours3; }
-				if (defined $hours4) { $chapel_foldbackSide{'hours4'} = $hours4; }
-			}
-			case m/gym_mainSide/ {
-				if (defined $status) { $gym_mainSide{'power'} = $status; }
-				if (defined $hours1) { $gym_mainSide{'hours1'} = $hours1; }
-				if (defined $hours2) { $gym_mainSide{'hours2'} = $hours2; }
-				if (defined $hours3) { $gym_mainSide{'hours3'} = $hours3; }
-				if (defined $hours4) { $gym_mainSide{'hours4'} = $hours4; }
-			}
-			case m/well_mainCenter/ {
-				if (defined $status) { $well_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $well_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $well_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $well_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $well_mainCenter{'hours4'} = $hours4; }
-			}
-			#case m/rmX_mainCenter/ {
-			#	if (defined $status) { $rmX_mainCenter{'power'} = $status; }
-			#	if (defined $hours1) { $rmX_mainCenter{'hours1'} = $hours1; }
-			#	if (defined $hours2) { $rmX_mainCenter{'hours2'} = $hours2; }
-			#	if (defined $hours3) { $rmX_mainCenter{'hours3'} = $hours3; }
-			#	if (defined $hours4) { $rmX_mainCenter{'hours4'} = $hours4; }
-			#}
-			case m/rm101A_mainCenter/ {
-				if (defined $status) { $rm101A_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm101A_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm101A_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm101A_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm101A_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm101C_mainCenter/ {
-				if (defined $status) { $rm101C_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm101C_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm101C_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm101C_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm101C_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm102_mainCenter/ {
-				if (defined $status) { $rm102_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm102_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm102_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm102_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm102_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm104_mainCenter/ {
-				if (defined $status) { $rm104_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm104_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm104_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm104_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm104_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm128_mainCenter/ {
-				if (defined $status) { $rm128_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm128_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm128_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm128_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm128_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm212_mainCenter/ {
-				if (defined $status) { $rm212_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm212_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm212_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm212_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm212_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm214_mainCenter/ {
-				if (defined $status) { $rm214_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm214_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm214_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm214_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm214_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rm216_mainCenter/ {
-				if (defined $status) { $rm216_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rm216_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rm216_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rm216_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rm216_mainCenter{'hours4'} = $hours4; }
-			}
-			case m/rmRR1_mainCenter/ {
-				if (defined $status) { $rmRR1_mainCenter{'power'} = $status; }
-				if (defined $hours1) { $rmRR1_mainCenter{'hours1'} = $hours1; }
-				if (defined $hours2) { $rmRR1_mainCenter{'hours2'} = $hours2; }
-				if (defined $hours3) { $rmRR1_mainCenter{'hours3'} = $hours3; }
-				if (defined $hours4) { $rmRR1_mainCenter{'hours4'} = $hours4; }
-			}
-		}
 		# return our temporary hash,
 		# just for the fun of it:
 		return %hashName;
-
-		#print %hashName;
 }
 
 sub printStuff() {
@@ -340,45 +208,57 @@ sub printStuff() {
 	# print stuff out to JavaScript
 
 	if (defined $bridge_mainLeft{'power'}) {
-		print 'var bridge_mainLeft_power = '.$bridge_mainLeft{"power"}."\n";
+		print 'var bridge_mainLeft_power = '.$bridge_mainLeft{'power'}."\n";
 	} else {
 		print 'var bridge_mainLeft_power = GEEK_ERR'."\n";
 	}
 
 	if (defined $bridge_mainRight{'power'}) {
-		print 'var bridge_mainRight_power = '.$bridge_mainRight{"power"}."\n";
+		print 'var bridge_mainRight_power = '.$bridge_mainRight{'power'}."\n";
 	} else {
 		print 'var bridge_mainRight_power = GEEK_ERR'."\n";
 	}
 
 	if (defined $bridge_mainCenter{'power'}) {
-		print 'var bridge_mainCenter_power = '.$bridge_mainCenter{"power"}."\n";
+		print 'var bridge_mainCenter_power = '.$bridge_mainCenter{'power'}."\n";
 	} else {
 		print 'var bridge_mainCenter_power = GEEK_ERR'."\n";
 	}
 
 	if (defined $bridge_foldbackCenter{'power'}) {
-		print 'var bridge_foldbackCenter_power = '.$bridge_foldbackCenter{"power"}."\n";
+		print 'var bridge_foldbackCenter_power = '.$bridge_foldbackCenter{'power'}."\n";
 	} else {
 		print 'var bridge_foldbackCenter_power = GEEK_ERR'."\n";
 	}
 
 	if (defined $chapel_mainSide{'power'}) {
-		print 'var chapel_mainSide_power = '.$chapel_mainSide{"power"}."\n";
+		print 'var chapel_mainSide_power = '.$chapel_mainSide{'power'}."\n";
 	} else {
 		print 'var chapel_mainSide_power = GEEK_ERR'."\n";
 	}
 	
 	if (defined $chapel_mainCenter{'power'}) {
-		print 'var chapel_mainCenter_power = '.$chapel_mainCenter{"power"}."\n";
+		print 'var chapel_mainCenter_power = '.$chapel_mainCenter{'power'}."\n";
 	} else {
 		print 'var chapel_mainCenter_power = GEEK_ERR'."\n";
 	}
 
 	if (defined $chapel_foldbackSide{'power'}) {
-		print 'var chapel_foldbackSide_power = '.$chapel_foldbackSide{"power"}."\n";
+		print 'var chapel_foldbackSide_power = '.$chapel_foldbackSide{'power'}."\n";
 	} else {
 		print 'var chapel_foldbackSide_power = GEEK_ERR'."\n";
+	}
+
+	if (defined $rmXX_mainCenter{'power'}) {
+		print 'var rmXX_mainCenter_power = '.$rmXX_mainCenter{'power'}."\n";
+	} else {
+		print 'var rmXX_mainCenter_power = GEEK_ERR'."\n";
+	}
+
+	if (defined $rmXX_mainCenter{'power'}) {
+		print 'var rmXX_mainCenter_power = '.$rmXX_mainCenter{'power'}."\n";
+	} else {
+		print 'var rmXX_mainCenter_power = GEEK_ERR'."\n";
 	}
 }
 
