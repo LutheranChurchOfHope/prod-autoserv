f1d4bdc340eb5c1bb8b33ab91d83377c6857b7ae
Jared Brees <j8t8b@yahoo.com>
2016-07-02 15:04:30 -0500

    change time
    as usual: lotta changes, nothing tested.

--- Client-side/javascript/misc_func.js
+++ Client-side/javascript/misc_func.js
@@ -1,13 +1,49 @@
-var bridge_mainLeft_indicator;
-var bridge_mainRight_indicator;
-var bridge_mainCenter_indicator;
-var bridge_foldbackCenter_indicator;
-var chapel_mainSide_indicator;
-var chapel_mainCenter_indicator;
-var chapel_foldbackSide_indicator;
-var gym_mainSide_indicator;
-var well_mainCenter_indicator;
+// power
+var bridge_mainLeft_power = 'GEEK_ERR';
+var bridge_mainRight_power = 'GEEK_ERR';
+var bridge_mainCenter_power = 'GEEK_ERR';
+var bridge_foldbackCenter_power = 'GEEK_ERR';
+var chapel_mainSide_power = 'GEEK_ERR';
+var chapel_mainCenter_power = 'GEEK_ERR';
+var chapel_foldbackSide_power = 'GEEK_ERR';
+var gym_mainSide_power = 'GEEK_ERR';
+var well_mainCenter_power = 'GEEK_ERR';
+// hours
+var bridge_mainLeft_hours = 'GEEK_ERR';
+var bridge_mainRight_hours = 'GEEK_ERR';
+var bridge_mainCenter_hours = 'GEEK_ERR';
+var bridge_foldbackCenter_hours = 'GEEK_ERR';
+var chapel_mainSide_hours = 'GEEK_ERR';
+var chapel_mainCenter_hours = 'GEEK_ERR';
+var chapel_foldbackSide_hours = 'GEEK_ERR';
+var gym_mainSide_hours = 'GEEK_ERR';
+var well_mainCenter_hours = 'GEEK_ERR';
+// indicator
+var bridge_mainLeft_indicator = 'GEEK_ERR';
+var bridge_mainRight_indicator = 'GEEK_ERR';
+var bridge_mainCenter_indicator = 'GEEK_ERR';
+var bridge_foldbackCenter_indicator = 'GEEK_ERR';
+var chapel_mainSide_indicator = 'GEEK_ERR';
+var chapel_mainCenter_indicator = 'GEEK_ERR';
+var chapel_foldbackSide_indicator = 'GEEK_ERR';
+var gym_mainSide_indicator = 'GEEK_ERR';
+var well_mainCenter_indicator = 'GEEK_ERR';
 
+var sdRED = '<img src="images/red-dot.png" alt="red-dot">';
+var sdORANGE = '<img src="images/flashing-orange.gif" alt="warning/error">';
+var sdGREEN = '<img src="images/green-dot.png" alt="green-dot">';
+
+var myVar = setInterval(doStuffOverAndOver,500);
+
+function doStuffOverAndOver() {
+	readVars();
+	runThings();
+}
+
+function runThings() {
+	powerToStatusDot();
+	replaceInnerHTML();
+}
 
 function replaceInnerHTML() {
 	/* Power statuses */
@@ -31,13 +67,66 @@ function replaceInnerHTML() {
 	document.getElementById("chapel_foldbackSide_hours").innerHTML = chapel_foldbackSide_hours;
 	document.getElementById("gym_mainSide_hours").innerHTML = gym_mainSide_hours;
 	document.getElementById("well_mainCenter_hours").innerHTML = well_mainCenter_hours;
+
+	/* Status indicators */
+	document.getElementById("bridge_mainLeft_indicator").innerHTML = bridge_mainLeft_indicator;
+	document.getElementById("bridge_mainRight_indicator").innerHTML = bridge_mainRight_indicator;
+	document.getElementById("bridge_mainCenter_indicator").innerHTML = bridge_mainCenter_indicator;
+	document.getElementById("bridge_foldbackCenter_indicator").innerHTML = bridge_foldbackCenter_indicator;
+	document.getElementById("chapel_mainSide_indicator").innerHTML = chapel_mainSide_indicator;
+	document.getElementById("chapel_mainCenter_indicator").innerHTML = chapel_mainCenter_indicator;
+	document.getElementById("chapel_foldbackSide_indicator").innerHTML = chapel_foldbackSide_indicator;
+	document.getElementById("gym_mainSide_indicator").innerHTML = gym_mainSide_indicator;
+	document.getElementById("well_mainCenter_indicator").innerHTML = well_mainCenter_indicator;
 	
 }
 
-function statusDot() {
-	if (bridge_mainLeft_power == "OFF") { bridge_mainLeft_indicator = '<img src="images/red-dot.png" alt="red-dot">'; }
+/*
+var indicatorStrings = [
+	bridge_mainLeft_indicator,
+	bridge_mainRight_indicator,
+	bridge_mainCenter_indicator,
+	bridge_foldbackCenter_indicator,
+	chapel_mainSide_indicator,
+	chapel_mainCenter_indicator,
+	chapel_foldbackSide_indicator,
+	gym_mainSide_indicator,
+	well_mainCenter_indicator,
+];
+*/
+
+function powerToStatusDot() {
+	/* Bridge */
+	bridge_mainLeft_indicator = statusDot(bridge_mainLeft_power);
+	bridge_mainRight_indicator = statusDot(bridge_mainRight_power);
+	bridge_mainCenter_indicator = statusDot(bridge_mainCenter_power);
+	bridge_foldbackCenter_indicator = statusDot(bridge_foldbackCenter_power);
+
+	/* Chapel */
+	chapel_mainSide_indicator = statusDot(chapel_mainSide_power);
+	chapel_mainCenter_indicator = statusDot(chapel_mainCenter_power);
+	chapel_foldbackSide_indicator = statusDot(chapel_foldbackSide_power);
+
+	/* Gym */
+	gym_mainSide_indicator = statusDot(gym_mainSide_power);
+
+	/* Well */
+	well_mainCenter_indicator = statusDot(well_mainCenter_power);
+}
+
+//indicatorStrings.forEach(statusDot);
+
+function statusDot(sdVal) { // "sd" = "status dot"
+	if (sdVal == "OFF") {
+		return sdRED;
+	}
 	else {
-		if (bridge_mainLeft_power == "ON") { bridge_mainLeft_indicator = '<img src="images/green-dot.png" alt="green-dot">'; }
-		else { bridge_mainLeft_indicator = '<img src="images/flashing-orange.gif" alt="warning/error">'; }
+		if (sdVal == "ON") {
+			return sdGREEN;
+		}
+		else {
+			return sdORANGE;
+		}
 	}
 }
+	
