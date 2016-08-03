e2b50faa011894a0a8b680f09aec00e4548e598a
cf8geek <cf8geek@users.noreply.github.com>
2016-08-02 19:06:59 -0500

    added other rooms

--- Client-side/javascript/misc_func.js
+++ Client-side/javascript/misc_func.js
@@ -8,6 +8,15 @@ var chapel_mainCenter_power = 'GEEK_ERR';
 var chapel_foldbackSide_power = 'GEEK_ERR';
 var gym_mainSide_power = 'GEEK_ERR';
 var well_mainCenter_power = 'GEEK_ERR';
+var rmRR1_mainCenter_power = 'GEEK_ERR';
+var rm128_mainCenter_power = 'GEEK_ERR';
+var rm101A_mainCenter_power = 'GEEK_ERR';
+var rm101C_mainCenter_power = 'GEEK_ERR';
+var rm102_mainCenter_power = 'GEEK_ERR';
+var rm104_mainCenter_power = 'GEEK_ERR';
+var rm212_mainCenter_power = 'GEEK_ERR';
+var rm214_mainCenter_power = 'GEEK_ERR';
+var rm216_mainCenter_power = 'GEEK_ERR';
 
 // hours
 var bridge_mainLeft_hours = 'GEEK_ERR';
@@ -19,6 +28,15 @@ var chapel_mainCenter_hours = 'GEEK_ERR';
 var chapel_foldbackSide_hours = 'GEEK_ERR';
 var gym_mainSide_hours = 'GEEK_ERR';
 var well_mainCenter_hours = 'GEEK_ERR';
+var rmRR1_mainCenter_hours = 'GEEK_ERR';
+var rm128_mainCenter_hours = 'GEEK_ERR';
+var rm101A_mainCenter_hours = 'GEEK_ERR';
+var rm101C_mainCenter_hours = 'GEEK_ERR';
+var rm102_mainCenter_hours = 'GEEK_ERR';
+var rm104_mainCenter_hours = 'GEEK_ERR';
+var rm212_mainCenter_hours = 'GEEK_ERR';
+var rm214_mainCenter_hours = 'GEEK_ERR';
+var rm216_mainCenter_hours = 'GEEK_ERR';
 
 // indicator
 var bridge_mainLeft_indicator = 'GEEK_ERR';
@@ -30,6 +48,15 @@ var chapel_mainCenter_indicator = 'GEEK_ERR';
 var chapel_foldbackSide_indicator = 'GEEK_ERR';
 var gym_mainSide_indicator = 'GEEK_ERR';
 var well_mainCenter_indicator = 'GEEK_ERR';
+var rmRR1_mainCenter_indicator = 'GEEK_ERR';
+var rm128_mainCenter_indicator = 'GEEK_ERR';
+var rm101A_mainCenter_indicator = 'GEEK_ERR';
+var rm101C_mainCenter_indicator = 'GEEK_ERR';
+var rm102_mainCenter_indicator = 'GEEK_ERR';
+var rm104_mainCenter_indicator = 'GEEK_ERR';
+var rm212_mainCenter_indicator = 'GEEK_ERR';
+var rm214_mainCenter_indicator = 'GEEK_ERR';
+var rm216_mainCenter_indicator = 'GEEK_ERR';
 
 var sdRED = '<img src="graphics/red-dot.png" alt="red-dot">';
 var sdORANGE = '<img src="graphics/flashing-orange.gif" alt="warning/error">';
@@ -74,6 +101,15 @@ function replaceInnerHTML() {
 	document.getElementById("chapel_foldbackSide_power").innerHTML = chapel_foldbackSide_power;
 	document.getElementById("gym_mainSide_power").innerHTML = gym_mainSide_power;
 	document.getElementById("well_mainCenter_power").innerHTML = well_mainCenter_power;
+	document.getElementById("rmRR1_mainCenter_power").innerHTML = rmRR1_mainCenter_power;
+	document.getElementById("rm128_mainCenter_power").innerHTML = rm128_mainCenter_power;
+	document.getElementById("rm101A_mainCenter_power").innerHTML = rm101A_mainCenter_power;
+	document.getElementById("rm101C_mainCenter_power").innerHTML = rm101C_mainCenter_power;
+	document.getElementById("rm102_mainCenter_power").innerHTML = rm102_mainCenter_power;
+	document.getElementById("rm104_mainCenter_power").innerHTML = rm104_mainCenter_power;
+	document.getElementById("rm212_mainCenter_power").innerHTML = rm212_mainCenter_power;
+	document.getElementById("rm214_mainCenter_power").innerHTML = rm214_mainCenter_power;
+	document.getElementById("rm216_mainCenter_power").innerHTML = rm216_mainCenter_power;
 	
 	/* Hours times */
 	document.getElementById("bridge_mainLeft_hours").innerHTML = bridge_mainLeft_hours;
@@ -85,6 +121,15 @@ function replaceInnerHTML() {
 	document.getElementById("chapel_foldbackSide_hours").innerHTML = chapel_foldbackSide_hours;
 	document.getElementById("gym_mainSide_hours").innerHTML = gym_mainSide_hours;
 	document.getElementById("well_mainCenter_hours").innerHTML = well_mainCenter_hours;
+	document.getElementById("rmRR1_mainCenter_hours").innerHTML = rmRR1_mainCenter_hours;
+	document.getElementById("rm128_mainCenter_hours").innerHTML = rm128_mainCenter_hours;
+	document.getElementById("rm101A_mainCenter_hours").innerHTML = rm101A_mainCenter_hours;
+	document.getElementById("rm101C_mainCenter_hours").innerHTML = rm101C_mainCenter_hours;
+	document.getElementById("rm102_mainCenter_hours").innerHTML = rm102_mainCenter_hours;
+	document.getElementById("rm104_mainCenter_hours").innerHTML = rm104_mainCenter_hours;
+	document.getElementById("rm212_mainCenter_hours").innerHTML = rm212_mainCenter_hours;
+	document.getElementById("rm214_mainCenter_hours").innerHTML = rm214_mainCenter_hours;
+	document.getElementById("rm216_mainCenter_hours").innerHTML = rm216_mainCenter_hours;
 
 	/* Status indicators */
 	document.getElementById("bridge_mainLeft_indicator").innerHTML = bridge_mainLeft_indicator;
@@ -96,6 +141,15 @@ function replaceInnerHTML() {
 	document.getElementById("chapel_foldbackSide_indicator").innerHTML = chapel_foldbackSide_indicator;
 	document.getElementById("gym_mainSide_indicator").innerHTML = gym_mainSide_indicator;
 	document.getElementById("well_mainCenter_indicator").innerHTML = well_mainCenter_indicator;
+	document.getElementById("rmRR1_mainCenter_indicator").innerHTML = rmRR1_mainCenter_indicator;
+	document.getElementById("rm128_mainCenter_indicator").innerHTML = rm128_mainCenter_indicator;
+	document.getElementById("rm101A_mainCenter_indicator").innerHTML = rm101A_mainCenter_indicator;
+	document.getElementById("rm101C_mainCenter_indicator").innerHTML = rm101C_mainCenter_indicator;
+	document.getElementById("rm102_mainCenter_indicator").innerHTML = rm102_mainCenter_indicator;
+	document.getElementById("rm104_mainCenter_indicator").innerHTML = rm104_mainCenter_indicator;
+	document.getElementById("rm212_mainCenter_indicator").innerHTML = rm212_mainCenter_indicator;
+	document.getElementById("rm214_mainCenter_indicator").innerHTML = rm214_mainCenter_indicator;
+	document.getElementById("rm216_mainCenter_indicator").innerHTML = rm216_mainCenter_indicator;
 	
 }
 
@@ -130,6 +184,17 @@ function powerToStatusDot() {
 
 	/* Well */
 	well_mainCenter_indicator = statusDot(well_mainCenter_power);
+
+	/* Classrooms */
+	rmRR1_mainCenter_indicator = statusDot(rmRR1_mainCenter_power);
+	rm128_mainCenter_indicator = statusDot(rm128_mainCenter_power);
+	rm101A_mainCenter_indicator = statusDot(rm101A_mainCenter_power);
+	rm101C_mainCenter_indicator = statusDot(rm101C_mainCenter_power);
+	rm102_mainCenter_indicator = statusDot(rm102_mainCenter_power);
+	rm104_mainCenter_indicator = statusDot(rm104_mainCenter_power);
+	rm212_mainCenter_indicator = statusDot(rm212_mainCenter_power);
+	rm214_mainCenter_indicator = statusDot(rm214_mainCenter_power);
+	rm216_mainCenter_indicator = statusDot(rm216_mainCenter_power);
 }
 
 //indicatorStrings.forEach(statusDot);
