687c4c154e938a0fab019bc41aec67623b01bf28
Jared Brees <j8t8b@yahoo.com>
2016-10-14 15:27:46 -0500

    bug fixes
    fixed some bugs, added more stuff

--- Automation/disable_auto-off.sh
+++ Automation/disable_auto-off.sh
@@ -2,6 +2,12 @@
 
 # location:
 # /var/www/cgi-bin/disable_auto-off.sh
+echo 'Content-type: text/html'
+echo -e "\n\n"
 
 cp /var/www/html/javascript/auto-off_disabled.txt /var/www/html/javascript/auto-off.js
+
+echo -e "\n\n" >> /opt/log/projector-automation.log
+date +%F_%H%M%S >> /opt/log/projector-automation.log
+echo "Disabled auto-off." >> /opt/log/projector-automation.log
 #EOF
--- Automation/enable_auto-off.sh
+++ Automation/enable_auto-off.sh
@@ -2,6 +2,12 @@
 
 # location:
 # /var/www/cgi-bin/enable_auto-off.sh
+echo 'Content-type: text/html'
+echo -e "\n\n"
 
 cp /var/www/html/javascript/auto-off_enabled.txt /var/www/html/javascript/auto-off.js
+
+echo -e "\n\n" >> /opt/log/projector-automation.log
+date +%F_%H%M%S >> /opt/log/projector-automation.log
+echo "Enabled auto-off." >> /opt/log/projector-automation.log
 #EOF
